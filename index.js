73cc42889fac110da28c5879439ba7b2bebac06d
KarinPortfolio <karin.abe@gmail.com>
2023-08-29 06:49:13 -0300

    criação de endpoint oi

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+/* cria app express */
 const express = require("express");
 const app = express();
-
+/* chama app */
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
+app.get("/oi", function (req, res) {
+  res.send("ola mundo");
+});
+
+/* app listen  - mostra o app*/
 app.listen(3000);
